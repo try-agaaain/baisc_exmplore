46665e6547f9f592d25394bbd430dba52f532da3
try-agaaain <2629757717@qq.com>
2024-06-23 19:24:11 +0800

    清理redis缓存
    + 通过@Schedule注解定期的清除redis中的缓存

--- redisson_basic/src/main/java/com/example/redissondemo/CacheService.java
+++ redisson_basic/src/main/java/com/example/redissondemo/CacheService.java
@@ -1,12 +1,16 @@
 package com.example.redissondemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBucket;
+import org.redisson.api.RKeys;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class CacheService {
 
@@ -21,5 +25,17 @@ public class CacheService {
     public String getCacheValue(String key) {
         RBucket<String> bucket = redissonClient.getBucket(key);
         return bucket.get();
+    }
+
+    @Scheduled(fixedRate = 6000) // 每60秒执行一次
+    public void clearExpiredCaches() {
+        log.info("Clear expired caches");
+        RKeys keys = redissonClient.getKeys();
+        // 使用模式匹配来删除一组特定的缓存键，例如以"temp:"开头的所有键
+        Iterable<String> targetedKeys = keys.getKeysByPattern("*");
+        for (String key : targetedKeys) {
+            log.info("delete key: {}", key);
+            keys.delete(key); // 删除匹配到的每个键
+        }
     }
 }
